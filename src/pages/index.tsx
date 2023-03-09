815f5f3371f7dcda788a42cc7659a2dfdb9f908f
cuatr0 <vanotsintsabadze16@gmail.com>
2023-03-10 02:29:09 +0400

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Main</title>
+        <title>VANO</title>
       </Head>
       <div className="w-[full ] h-[100vh] bg-[#2e2f31]">
         <div className="w-[full] h-full flex justify-center items-center flex-col">
